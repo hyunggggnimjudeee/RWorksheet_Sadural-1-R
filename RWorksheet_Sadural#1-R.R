425953f3bd33a5e828c1a898eab5f6432d0a6f38
Ryan Jude Sadural <saduralryanjude@gmail.com>
2023-09-20 04:04:33 +0000

    1

---
+++ RWorksheet_Sadural#1-R.R
@@ -0,0 +1,31 @@
+ageVector <-  c(34,28,22,36,27,18,52,39,42,29,35,31,27,22,37,34,19,20,57,49,50,37,46,25,17,37,42,53,41,51,35,24,33,41)
+
+length (ageVector)
+
+#2
+reciprocalAge <- 1/ageVector
+reciprocalAge
+
+#3 Assign also new_age <- c(age,0,age).
+#3 What happen to the new age?
+
+new_age <-c(ageVector,0,ageVector)
+new_age
+
+#4 Sort the values of age
+
+SortedageVector <- c(ageVector)
+SortedageVector
+
+#5 Find the minimum and maximum value for age
+
+min(ageVector)
+max(ageVector)
+
+#6. 
+
+decimalVector <- c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7)
+
+length (decimalVector)
+decimalVector *2
+
