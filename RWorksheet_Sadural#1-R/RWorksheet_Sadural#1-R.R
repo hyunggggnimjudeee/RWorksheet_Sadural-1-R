62176abfaeb2e3f3bed4683f23298b33e4e2fe29
Ryan Jude Sadural <saduralryanjude@gmail.com>
2023-09-28 03:54:00 +0000

    Updated Commit

---
+++ RWorksheet_Sadural#1-R/RWorksheet_Sadural#1-R.R
@@ -0,0 +1,113 @@
+ageVector <-  c(34,28,22,36,27,18,52,39,42,29,35,31,27,22,37,34,19,20,57,49,50,37,46,25,17,37,42,53,41,51,35,24,33,41)
+
+length (ageVector)
+
+#2
+reciprocalAge <- 1/ageVector
+reciprocalAge
+
+#3 Assign also new_age <- c(age,0,age).
+#3 What happen to the new age?
+
+new_age <-c(ageVector,0,ageVector)
+new_age
+
+#4 Sort the values of age
+
+SortedageVector <- c(ageVector)
+SortedageVector
+
+#5 Find the minimum and maximum value for age
+
+min(ageVector)
+max(ageVector)
+
+#6. 
+
+decimalVector <- c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7)
+
+length (decimalVector)
+#7.
+decimalVector *2
+
+#8.1 Integers from 1 to 100
+seq_Vec <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100.)
+
+length (seq_Vec)
+
+#8.2 Numbers 20 to 60
+
+seq_Vec <- c(20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60)
+
+#8.3 Means of 20 and 60
+mean(20+21+22+23+24+25+26+27+28+29+30+31+32+33+34+35+36+37+38+39+40+41+42+43+44+45+46+47+48+49+50+51+52+53+54+55+56+57+58+59+60)
+
+mean(20:60)
+
+#8.4 Sum of numbers from 51 to 91
+
+sum(51:91)
+
+#8.5 Integers from 1 to 1000
+
+num_Vec <- seq(1,1000)
+
+#9 *Print a vector with the integers between 1 and 100 that are not divisible by 3, 5 and 7 using filter option
+
+filter(function(i) { all(i %% c(3,5,7) != 0) }, seq(100))
+
+filter(num_Vec (1:100)  {all(i %% c(3,5,7) !=0) }, seq(100))
+
+#10 Generate a sequence backwards of the integers from 1 to 100.
+
+seq_Vec <- c(1:100)
+
+rev(seq_Vec)
+
+#11 List all the natural numbers below 25 that are multiples of 3 or 5.
+natural_Num <- seq(24,1)
+natural_Num2 <- Filter(function(i) { all(i %% (i %% 3 == 0 || i %% 5 == 0) != 0) } ,seq(24,1))
+natural_Num2 <- c(natural_Num2)
+natural_Sum <- sum(natural_Num2)
+natural_Sum
+
+#11a data points from 10 to 11
+ten_11dtps <- length(rev(seq_Vec)) + length(natural_Sum) + length(natural_Num2)
+ten_11dtps
+
+#12
+{x <- 0+ x+ 5+}
+
+
+#it is error because the plus sign after the number 5 and it lacks proper context and structure
+#the statement should be:
+# {
+#   x <- 0
+#   x <- x + 5
+# }
+   
+#13 set vectorname score
+score <- c(72,86,92,63,88,91,92,75,75,77)
+
+score
+score_2 <- score[2]
+score_3 <- score[3]
+score_2
+score_3
+
+#14
+vector_1 <- c(1,2,NA,4,NA,6,7)
+
+#14a
+print(vector_1,na.print="999")
+
+#the NA change into "999"
+
+#15
+
+name = readline(prompt="Input Your Name: ")
+
+age = readline(prompt="Input Your Age: ")
+
+print(paste("My Name is",name, "and I am",age , "Years Old."))
+print(R.version.string)
---
+++ RWorksheet_Sadural#2-R/RWorksheet_Sadural#2-R.R
@@ -0,0 +1,130 @@
+#1 create vectors using : operator
+sequence_1 <- -5:5
+sequence_1
+
+#1b
+x <- 1:7
+x
+
+#2 create a vector using seq() function
+vector <- seq(1, 3, by = 0.2)
+vector
+
+#2 - From 1 we add, 0.2 until it reaches 3 so that it will come up into a answer of 3.0
+
+#3 a factory has census of its workers. There are 50 workers in total.
+ages <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 43, 53, 41, 51, 35, 24, 33, 41, 53, 40, 18, 44, 38, 41, 48, 27, 39, 19, 30, 61, 54, 58, 26, 18)
+
+#a. Access the 3rd Element
+element_3 <- ages[3]
+element_3
+
+#b. Access the 2nd and 4th elements
+element_2_and_4 <- ages[c(2,4)]
+element_2_and_4
+
+#c. Access all elements except the 4th and 12th
+filtered_ages <- ages[-c(4,12)]
+filtered_ages
+
+#4 *Create a vector x <- c("first"=3, "second"=0, "third"=9). Then named the vectors, names(x).
+x <- c("first" = 3, "second" = 0, "third" = 9)
+vector_names <- names(x)
+vector_names
+
+#5 Create a sequence x from -3:2.
+x <- -3:2
+#5a.
+x[2] <-0
+x
+
+#6 The following data shows the diesel fuel purchased by Mr. Cruz.
+month <- c("January", "February", "March", "April", "May")
+price_per_liter <- c(45.5, 46.2, 47.0, 46.8, 47.5)
+purchase_quantity <- c(100, 120, 110, 105, 115)
+
+fuel_purchases <- data.frame(
+  Month = month,
+  Price_Per_Liter_PHP = price_per_liter,
+  Purchase_Quantity_Liters = purchase_quantity)
+
+fuel_purchases
+
+#6b.
+months <- c("January", "February", "March", "April", "May", "June")
+price_per_liter <- c(45.5, 46.2, 47.0, 46.8, 47.5, 48.0)
+purchase_quantity <- c(100, 120, 110, 105, 115, 125)
+
+average_expenditure <- weighted.mean(price_per_liter, purchase_quantity)
+
+cat("The average fuel expenditure from January to June is:", average_expenditure, "PHP\n")
+
+#7 R has actually lots of built-in datasets. For example, the rivers data “gives the lengths(in miles) of 141 “major” rivers in North America, as compiled by the US Geological Survey”.)
+
+data(rivers)
+
+#7a. 
+data <- c(
+  length(rivers), 
+  sum(rivers), 
+  mean(rivers),
+  median(rivers),
+  var(rivers),
+  sd(rivers),
+  min(rivers),
+  max(rivers)
+)
+
+data
+
+#8 The table below gives the 25 most powerful celebrities and their annual pay as ranked by the editions of Forbes magazine and as listed on the Forbes.com website.
+
+#8a
+celebrities <- c(
+  "Taylor Swift", "Kylie Jenner", "Kanye West", "Lionel Messi", "Ed Sheeran",
+  "Cristiano Ronaldo", "Neymar", "Howard Stern", "LeBron James", "Dwayne Johnson",
+  "Rush Limbaugh", "Ellen DeGeneres", "Elton John", "Ariana Grande", "Ryan Reynolds",
+  "Jonas Brothers", "Jennifer Lopez", "Shawn Mendes", "Billie Eilish", "Tennis Players",
+  "JK Rowling", "BTS", "Dr. Phil McGraw", "Travis Scott", "Shawn Mendes"
+)
+
+pay <- c(
+  185, 170, 150, 127, 110,
+  109, 105, 93, 89, 89,
+  87, 80, 84, 72, 71,
+  68, 58, 58, 53, 53,
+  92, 170, 95, 58, 38
+)
+
+celebrities
+pay
+
+#8b
+celebrities[21] <- "J.K. Rowling"
+pay[21] <- 90
+
+celebrities
+pay
+
+#8c
+celeb_data <- data.frame(Celebrity = celebrities, Pay = pay)
+library(openxlsx)
+library(writexl)
+
+library(readxl)
+celeb_data <- read_excel("PowerRanking.xlsx", sheet = "Celebrities")
+write.csv(celeb_data, file = "PowerRanking.csv", row.names = FALSE)
+
+#8d.
+subset_data <- celeb_data[10:20, ]
+save(subset_data, file = "Ranks.RData")
+
+#8e.
+#To describe the output of each step, I'll provide a brief summary:
+
+#In step (a), we created two vectors, one for celebrities and another for their annual pay.
+#In step (b), we modified the power ranking and pay of J.K. Rowling in the vectors.
+#In step (c), we created an Excel file "PowerRanking.xlsx" from the data and then converted it to a CSV file "PowerRanking.csv."
+#In step (d), we extracted rows 10 to 20 from the data and saved it as "Ranks.RData," which is an RData file containing this subset of data.
+
+#9.
